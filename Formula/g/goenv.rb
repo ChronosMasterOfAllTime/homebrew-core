e591a0c00edded5b4b41feb266f1e46daa1a475b
Efstathios Chouliaris <Efstathios.Chouliaris@thomsonreuters.com>
2026-04-13 13:30:16 -0500

    goenv 3.0.1
    Update goenv to v3, a Go-based rewrite of the version manager.
    Add goenv@3 alias for the new major version.

--- Formula/g/goenv.rb
+++ Formula/g/goenv.rb
@@ -1,11 +1,12 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/go-nv/goenv"
-  url "https://github.com/go-nv/goenv/archive/refs/tags/2.2.39.tar.gz"
-  sha256 "9c5571e731c1bbf4f6cc1d3606da1960ba56cf0ce1206c58a74e52e6df430e35"
+  url "https://github.com/go-nv/goenv/archive/refs/tags/3.0.1.tar.gz"
+  sha256 "ecaefc6c98cb53c4dd53b86dc604e9e9c2fcafc281c8e3c24e9070365f61cc41"
   license "MIT"
   version_scheme 1
-  head "https://github.com/go-nv/goenv.git", branch: "master"
+  # TODO: Uncomment when default branch is changed from 'master' to 'main'
+  # head "https://github.com/go-nv/goenv.git", branch: "main"
 
   livecheck do
     url :stable
@@ -16,23 +17,19 @@ class Goenv < Formula
     sha256 cellar: :any_skip_relocation, all: "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
   end
 
-  def install
-    inreplace_files = [
-      "libexec/goenv",
-      "plugins/go-build/install.sh",
-      "test/goenv.bats",
-      "test/test_helper.bash",
-      "plugins/go-build/test/test_helper.bash",
-    ]
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+  depends_on "go" => :build
 
-    prefix.install Dir["*"]
-    %w[goenv-install goenv-uninstall go-build].each do |cmd|
-      bin.install_symlink "#{prefix}/plugins/go-build/bin/#{cmd}"
-    end
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap&.user || "homebrew"}
+      -X main.buildTime=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"goenv")
   end
 
   test do
-    assert_match "Usage: goenv <command> [<args>]", shell_output("#{bin}/goenv help")
+    assert_match "no Go versions installed yet", shell_output("#{bin}/goenv list 2>&1", 1)
   end
 end
